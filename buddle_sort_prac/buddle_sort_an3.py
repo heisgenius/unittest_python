f8f587faaf31298ff6b4e380f6c4e448de2966a4
heisgenius <250118550@qq.com>
2019-11-14 15:26:45 +0800

    ‘unittest’

--- buddle_sort_prac/buddle_sort_an3.py
+++ buddle_sort_prac/buddle_sort_an3.py
@@ -1,7 +1,9 @@
 def bubble_sort(nums):
-    for i in range(len(nums) - 1):  # 这个循环负责设置冒泡排序进行的次数
+    n = len(nums)
+    for i in range(n - 1):  # 这个循环负责设置冒泡排序进行的次数
         status = False
-        for j in range(len(nums) - i - 1):  # j为列表下标
+        x = n - i -1
+        for j in range(n - i - 1):  # j为列表下标
             if nums[j] > nums[j + 1]:
                 nums[j], nums[j + 1] = nums[j + 1], nums[j]
                 status = True
--- buddle_sort_prac/buddle_sort_p.py
+++ buddle_sort_prac/buddle_sort_p.py
@@ -5,21 +5,25 @@ def bubble_sort(the_list):
     i = 0
     while i < len(the_list):
         j = 0
+        status = False
         while j < len(the_list) - 1:
-            print(the_list[j], the_list[j + 1])
+            # print(the_list[j], the_list[j + 1])
             if the_list[j] > the_list[j + 1]:
                 the_list[j], the_list[j + 1] = the_list[j + 1], the_list[j]
             j = j + 1
-            print(the_list)
+            status = True
+        if not status:
+            return the_list
+            # print(the_list)
 
-        print("======" + str(the_list))
+        # print("======" + str(the_list))
 
         i = i + 1
     return the_list
 
 
 if __name__ == '__main__':
-    the_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    the_list = [1, 2, 767, 4, 5, 6, 7, 8, 9, 10]
     print("排序前：" + str(the_list))
 
     print("排序后：" + str(bubble_sort(the_list)))
